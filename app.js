d2ffb6a3ae2ef0a696988beaed54e6a24fc9aab6
luna9862 <lunaest26@gmail.com>
2024-07-15 14:37:38 -0400

    fixed problem seq

--- app.js
+++ app.js
@@ -2,32 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const { Sequelize, DataTypes } = require('sequelize');
+const { FavoritePair} = require('./index');
 
 // Initialize Express app
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Initialize Sequelize
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
-});
-
-// Define FavoritePair model
-const FavoritePair = sequelize.define('FavoritePair', {
-  baseCurrency: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  targetCurrency: {
-    type: DataTypes.STRING,
-    allowNull: false
-  }
-});
-
-// Sync the database
-sequelize.sync();
 
 const API_KEY = 'fca_live_MXik8dH6uun2VfClx6vu54cZtJ3qfsTGmvYxueRn';
 const BASE_URL = 'https://api.freecurrencyapi.com/v1/';
@@ -49,7 +30,6 @@ app.get('/api/exchange-rate/:base', async (req, res) => {
 });
 
 // Fetch historical exchange rates
-// Fetch historical exchange rates
 app.get('/api/historical-rate/:base/:target/:date', async (req, res) => {
   const { base, target, date } = req.params;
   try {
@@ -60,7 +40,6 @@ app.get('/api/historical-rate/:base/:target/:date', async (req, res) => {
         date
       }
     });
-    console.log(response.data); // Log the response data
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch historical rates' });
